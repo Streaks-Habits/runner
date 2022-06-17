192fa1b600ce6ecd36678231741b630cf9a5ef92
Ethan PEGEOT <git@coldsurikat.fr>
2022-06-17 19:49:11 +0200

    📦 Working strava runner

---
+++ daemons/strava/README.md
@@ -0,0 +1,6 @@
+# Strava Runner
+
+This little script checks if an activity has been registered "today" and runs if HOUR = 23.
+
+## Arguments:
+- JSON Object with {athlete_id : INT OR STR}
--- daemons/strava/main.py
+++ daemons/strava/main.py
@@ -1,14 +1,38 @@
 import requests
 import json
 import sys
+import datetime
 from bs4 import BeautifulSoup
 
-html_text = requests.get("https://www.strava.com/athletes/14563597").text
+def log(t):
+	print("[ERROR] ", t)
+	exit(1)
+
+if datetime.datetime.now().hour != 23:
+	exit(0)
+
+if len(sys.argv) != 2:
+	log("Not enough OR too much arguments, waits only the athlete_id in JSON format")
+parameters = json.loads(sys.argv[1])
+
+profile_url = "https://www.strava.com/athletes/" + str(parameters["athlete_id"])
+html_text = requests.get(profile_url).text
 soup = BeautifulSoup(html_text, 'html.parser')
 
 user_data_text = soup.find_all('div', attrs={
 		'data-react-class': 'AthleteProfileApp'
 	})[0].get('data-react-props')
-user_data = json.loads(user_data_text)
+user_data = json.loads(user_data_text)['recentActivities']
 
-print(user_data["recentActivities"])
+if not user_data:
+	log("No access to profile")
+
+moved_his_ass = False
+for activity in user_data:
+	if activity['startDateLocal'] == "Today":
+		moved_his_ass = True
+
+if moved_his_ass:
+	print("success")
+else:
+	log("No acitivty today")
