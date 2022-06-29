c03b87d6d641647aab568032bdb6a9db20eee606
Olivier Cartier <me@cestoliv.com>
2022-06-29 14:09:31 +0200

    👌 improve: ability to specify activities types

--- daemons/strava/main.py
+++ daemons/strava/main.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import sys
-import datetime
 from bs4 import BeautifulSoup
 
 def log(t):
@@ -9,27 +8,32 @@ def log(t):
 	exit(1)
 
 if len(sys.argv) != 2:
-	log("Not enough OR too much arguments, waits only the athlete_id in JSON format")
-parameters = json.loads(sys.argv[1])
+	log('Usage: python main.py \'{"athlete_id": "<athlete_id>", "activities": ["<activities>"]}\'')
 
-profile_url = "https://www.strava.com/athletes/" + str(parameters["athlete_id"])
-html_text = requests.get(profile_url).text
-soup = BeautifulSoup(html_text, 'html.parser')
+settings = json.loads(sys.argv[1])
+if not 'athlete_id' in settings or not 'activities' in settings:
+	log('Usage: python main.py \'{"athlete_id": "<athlete_id>", "activities": ["<activities>"]}\'')
 
-user_data_text = soup.find_all('div', attrs={
-		'data-react-class': 'AthleteProfileApp'
-	})[0].get('data-react-props')
-user_data = json.loads(user_data_text)['recentActivities']
+profile_url = "https://www.strava.com/athletes/" + str(settings["athlete_id"])
+profile_text = requests.get(profile_url).text
+profile_soup = BeautifulSoup(profile_text, 'html.parser')
+
+user_profile = profile_soup.find_all('div', attrs={'data-react-class': 'AthleteProfileApp'})
+if not user_profile:
+	log("Can't find user")
+user_profile = user_profile[0].get('data-react-props')
+
+user_data = json.loads(user_profile)['recentActivities']
 
 if not user_data:
-	log("No access to profile")
+	log("Can't access user activities")
 
-moved_his_ass = False
+moved_his_ass = 0
 for activity in user_data:
-	if activity['startDateLocal'] == "Today":
-		moved_his_ass = True
+	# If activity type is in the activities or if the activities array is empty
+	if activity['type'] in settings['activities'] or len(settings["activities"]) == 0:
+		if activity['startDateLocal'] == "Today":
+			moved_his_ass += 1
 
-if moved_his_ass:
+if moved_his_ass >= len(settings["activities"]):
 	print("success")
-else:
-	log("No acitivty today")
